1e99bbc6609ae982dea8e274350cffbbee893941
Dmitry Sergeev <identw@gmail.com>
2023-06-30 18:41:50 +0400

    Add support safari

---
+++ safari/Github Actions Flexible Interface/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Github Actions Flexible Interface
+//
+//  Created by Dmitry Sergeev on 08.06.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
